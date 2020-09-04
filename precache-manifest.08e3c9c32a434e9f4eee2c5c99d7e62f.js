a90c8d71a6824871c696ac25d599cece16d88b5c
Nikki Sharpley <nikkisharpley@gmail.com>
2020-09-04 18:47:26 -0400

    Updates

---
+++ precache-manifest.08e3c9c32a434e9f4eee2c5c99d7e62f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301368bfad1c3d512c8fe95ee4a66771",
+    "url": "/the-shoppies/index.html"
+  },
+  {
+    "revision": "54bfbc69c29edc30f1a2",
+    "url": "/the-shoppies/static/css/main.52b0b750.chunk.css"
+  },
+  {
+    "revision": "99a38115412d757fe462",
+    "url": "/the-shoppies/static/js/2.d281a46b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-shoppies/static/js/2.d281a46b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54bfbc69c29edc30f1a2",
+    "url": "/the-shoppies/static/js/main.f6748718.chunk.js"
+  },
+  {
+    "revision": "3028237cd38fe8ab6725",
+    "url": "/the-shoppies/static/js/runtime-main.7a5fa803.js"
+  },
+  {
+    "revision": "286069001111ea3ed233483011ce0c09",
+    "url": "/the-shoppies/static/media/Icon-search.28606900.svg"
+  },
+  {
+    "revision": "84107fd85132e111cde476f4b4d16662",
+    "url": "/the-shoppies/static/media/cinema.84107fd8.svg"
+  },
+  {
+    "revision": "2524395efe5498033af6685e9e428454",
+    "url": "/the-shoppies/static/media/goal.2524395e.svg"
+  },
+  {
+    "revision": "a8752ad47beced08cac37b609c5d31d3",
+    "url": "/the-shoppies/static/media/mail.a8752ad4.svg"
+  }
+]);
